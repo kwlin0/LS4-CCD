7c76b6e301f74ed242d19b616a0acf37fc904ad1
Kenneth Lin <kwlin@berkeley.edu>
2025-03-12 23:30:22 -0700

    Update fpack.py

--- scripts/fpack.py
+++ scripts/fpack.py
@@ -19,25 +19,62 @@ import argparse
 from astropy.io import fits
 from glob import glob
 
-# sample PrimaryHDU header
-hdu_0 = {'OBSMODE' : ['survey', 'Observing mode'],
-         'FOCUS'   : [28.10000, 'Telescope focus position'],
-         'INSTRUME': ['LS4Cam', 'La Silla Schmidt Southern Survey Camera'],
-         'OBSERVAT': ['ESO La Silla', 'ESO La Silla Observatory'],
-         'TELESCOP': ['ESO 1.0-m Schmidt'],
-         'LATITUDE': [-29.25444, 'Telescope Latitude  (degrees north)'],
-         'LONGITUD': [70.74167, 'Telescope Longitude (degrees west)'],
-         'ELEVATIO': [2347, 'Telescope Elevation (meters)'],
-         'CCDMODE' : ['dual', 'Dual or single amplifier readout mode'] }
+# Header entries
+basic_info = {
+                'INSTRUME': ['LS4Cam', 'La Silla Schmidt Southern Survey Camera'],
+                'OBSERVAT': ['ESO La Silla', 'ESO La Silla Observatory'],
+                'TELESCOP': ['ESO 1.0-m Schmidt'],
+                'LATITUDE': [-29.25444, 'Telescope Latitude  (degrees north)'],
+                'LONGITUD': [70.74167, 'Telescope Longitude (degrees west)'],
+                'ELEVATIO': [2347, 'Telescope Elevation (meters)'],
+                'CCDMODE' : ['dual', 'Dual or single amplifier readout mode'], 
+                'DATASECL': ['[7:1030,1:4096]', 'Data section from amp L'],
+                'BIASSECL' : ['[1031:1050,1:4096]', 'Overscan region L'],
+                'PRESECL' : ['[1:6,1:4096]', 'Prescan region L'],
+                'DATASECR' : ['[21:1044,1:4096]', 'Data section from amp R'],
+                'BIASSECR' : ['[1:20,1:4096]', 'Overscan region R'],
+                'PRESECR' : ['[1045:1050,1:4096]', 'Prescan region R']
+             }
 
-def modhead(hdu, header_dict):
+dynamic_info = {
+                'PROJECT' : ['LEG', 'determines whether public or proprietary'],
+                'TARGETID' : [0000, 'Target or field name'] 
+               }
+
+left_amp   = {
+                'BIASSEC' : ['[1031:1050,1:4096]', 'Overscan region L']
+             }
+
+right_amp  = {
+                'BIASSEC' : ['[1:20,1:4096]', 'Overscan region R']
+             }
+
+def modhead(hdu, header_dict, loc=None):
     """
     Modify FITS header given a FITS HDU and header data structured as a dictionary `header_dict`
+    By default, the new header entries are appended at the end of the header file. Optionally, use `loc`
+    argument to specify where the insert the header entry. Note FITS headers are zero-indexed from the top.
     """
+    nloc = 0
     for k,v in header_dict.items():
-        hdu.header[k] = v[0]
-        if len(v) == 2:
-            hdu.header.comments[k] = v[1]
+        # First check if keyword already exists. If it does, update keyword in place.
+        if k in hdu.header:
+            hdu.header[k] = v[0]
+            if len(v) == 2:
+                hdu.header.comments[k] = v[1]
+        # For new keywords, give option loc of where to put them in the header.
+        else:
+            if loc is not None:
+                if len(v) == 1:
+                    hdu.header.insert(loc+nloc, (k, v[0]))
+                elif len(v) == 2:
+                    hdu.header.insert(loc+nloc, (k, v[0], v[1]))
+                nloc += 1
+            # Otherwise append the new keywords at the end of the header.
+            else:
+                hdu.header[k] = v[0]
+                if len(v) == 2:
+                    hdu.header.comments[k] = v[1]
 
 def combine_files(single_exposure_filenames, compress=True, primaryheader=None):
     primaryHDU = fits.PrimaryHDU()
@@ -46,12 +83,17 @@ def combine_files(single_exposure_filenames, compress=True, primaryheader=None):
     hdulist = [primaryHDU]
     for filenum in range(len(single_exposure_filenames)):
         data       = fits.open(single_exposure_filenames[filenum])[0].data
-        header     = fits.open(single_exposure_filenames[filenum])[0].header
+        hdu_header = fits.open(single_exposure_filenames[filenum])[0].header 
         if compress == False:
-            imageHDU   = fits.ImageHDU(data, header=header)
+            imageHDU   = fits.ImageHDU(data, header=hdu_header)
         else:
-            imageHDU   = fits.CompImageHDU(data, header=header, compression_type='RICE_1')
-        hdulist.append(imageHDU)
+            imageHDU   = fits.CompImageHDU(data, header=hdu_header, compression_type='RICE_1')
+        
+        ## Header additions and updating ##
+        modhead(imageHDU, basic_info, loc=8)
+        modhead(imageHDU, dynamic_info, loc=21)
+
+        hdulist.append(imageHDU)  
     allHDU = fits.HDUList(hdulist)
     if compress == False:
         outname = os.path.dirname(single_exposure_filenames[0]) + ".fits"
@@ -65,6 +107,11 @@ def combine_files(single_exposure_filenames, compress=True, primaryheader=None):
         print("Wrote", outname, f"@ {round(size/(pow(1024,2)), 2)} MB")
     
 def main():
+    """
+    Script can be used in one of two ways:
+    1. To compress one exposure, the inputs should be the 64 FITS files from the controller.
+    2. To compress many exposures in one run, give the exposure directories with (or without) a wildcard.
+    """
     parser = argparse.ArgumentParser(description="Combine a single exposure focal plane image FITS files into a single fpacked file. Output files in the parent directory of raw image files.")
     parser.add_argument("path", nargs='+', help="Raw file input: (1) to compress single exposure, use path/to/exp_00000/*.fits, (2) to compress a number of exposures, use path/to/exp*")
     args = parser.parse_args()
@@ -73,7 +120,7 @@ def main():
     
     if inputdat[0].lower().endswith(('.fits', '.fit', '.FITS')):
         print('FITS files given\ncompressing as single exposure')
-        combine_files(inputdat)
+        combine_files(inputdat, primaryheader=basic_info)
     
     else:
         print('Compressing all exposures...')
@@ -82,7 +129,7 @@ def main():
             if os.path.isdir(file):
                 print(file)
                 single_exposure_filenames = sorted(glob(file+'/test*.fits'))
-                combine_files(single_exposure_filenames, primaryheader=hdu_0)
+                combine_files(single_exposure_filenames, primaryheader=basic_info)
             # If not a directory, skip
             else:
                 print('Skipping', file)
